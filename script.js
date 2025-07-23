e0aafb31478ff44212dba1ad999c00d09a349607
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-07-23 14:49:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	let word = '';
+	for (let i=0; i<s.length; i++) {
+		if (s[i] == ' ') {
+			break;
+		} 
+		word += s[i];
+	}
+	return word;
 }
 
 // Do not change the code below
