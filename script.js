bc75aa7fabbc922065fce69ab5c37f8ba327dbce
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-07-23 14:58:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function firstWord(s) {
   // your code here
 	let word = '';
-	for (let i=0; i<s.length; i++) {
-		if (s[i] == ' ') {
-			break;
-		} 
-		word += s[i];
+	// for (let i=0; i<s.length; i++) {
+	// 	if (s[i] == ' ') {
+	// 		break;
+	// 	} 
+	// 	word += s[i];
+	// }
+	while (i < s.length && s[i] === ' ') {
+	    i++;
 	}
-	return word;
-}
 
+   while (i < s.length && s[i] !== ' ') {
+	 word += s[i];
+	 i++;
+   }
+   return word;
+}
+  
 // Do not change the code below
 
 const s = prompt("Enter String:");
